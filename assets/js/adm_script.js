5ccd24a0fde528346d27e7a71a663b9110393b1a
Leandro PC Casa <lehnihon@hotmail.com>
2022-07-25 22:49:35 -0300

    warning alert

--- assets/js/adm_script.js
+++ assets/js/adm_script.js
@@ -1,16 +1,35 @@
-jQuery( function( $ ) {
-    $( 'input#_gift_card' ).change( function() {
-        var is_gift_card = $( 'input#_gift_card:checked' ).size();
+jQuery(function ($) {
+  function resetLens() {
+    $(".show_if_lens").hide();
+    $(".hide_if_lens").hide();
+  }
 
-        $( '.show_if_gift_card' ).hide();
-        $( '.hide_if_gift_card' ).hide();
+  function resetFrame() {
+    $(".show_if_frame").hide();
+    $(".hide_if_frame").hide();
+  }
 
-        if ( is_gift_card ) {
-            $( '.hide_if_gift_card' ).hide();
-        }
-        if ( is_gift_card ) {
-            $( '.show_if_gift_card' ).show();
-        }
-    });
-    $( 'input#_gift_card' ).trigger( 'change' );
+  $("input#_lens").change(function () {
+    var is_lens = $("input#_lens:checked").size();
+    resetLens();
+
+    if (is_lens) {
+      $(".hide_if_lens").hide();
+      $(".show_if_lens").show();
+      $("input#_frame").prop("checked", false);
+      resetFrame();
+    }
+  });
+  $("input#_frame").change(function () {
+    var is_frame = $("input#_frame:checked").size();
+    resetFrame();
+
+    if (is_frame) {
+      $(".hide_if_frame").hide();
+      $(".show_if_frame").show();
+      $("input#_lens").prop("checked", false);
+      resetLens();
+    }
+  });
+  $("input#_frame").trigger("change");
 });
--- includes/class-wc-wottica-admin.php
+++ includes/class-wc-wottica-admin.php
@@ -7,199 +7,160 @@ defined('ABSPATH') || exit;
  */
 class WC_Wottica_Admin
 {
+    protected $error_msg = '';
+
     public function __construct()
     {
-        add_action('init', [$this, 'register_custom_taxonomy'], 0);
+        add_action('init', [$this, 'session_start_admin']);
         add_action('woocommerce_product_after_variable_attributes', [$this, 'mytheme_woo_add_custom_variation_fields'], 10, 3);
         add_action('woocommerce_save_product_variation', [$this, 'mytheme_woo_add_custom_variation_fields_save'], 10, 2);
 
         add_filter('woocommerce_product_data_tabs', [$this, 'custom_product_tabs']);
-        add_filter('woocommerce_product_data_panels', [$this, 'giftcard_options_product_tab_content']);
-        add_action('woocommerce_process_product_meta_simple', [$this, 'save_giftcard_option_fields']);
-        add_action('woocommerce_process_product_meta_variable', [$this, 'save_giftcard_option_fields']);
-        add_filter('product_type_options', [$this,  'add_gift_card_product_option']);
+        add_filter('woocommerce_product_data_panels', [$this, 'extra_options_product_tab_content']);
+        add_action('woocommerce_process_product_meta_simple', [$this, 'save_extra_option_fields']);
+        add_action('woocommerce_process_product_meta_variable', [$this, 'save_extra_option_fields']);
+        add_filter('product_type_options', [$this,  'add_extra_product_option']);
+        add_action('admin_notices', [$this,  'my_admin_notices']);
     }
 
-    public function register_custom_taxonomy()
+    public function custom_product_tabs($tabs)
     {
-        $this->taxonomy_brand();
-        $this->taxonomy_material();
-    }
+        $tabs['lens'] = [
+            'label' => __('Lentes', 'woocommerce'),
+            'target' => 'lens_options',
+            'class' => ['show_if_lens'],
+        ];
 
-
-    function custom_product_tabs($tabs)
-    {
-
-        $tabs['giftcard'] = array(
-            'label'        => __('Lentes', 'woocommerce'),
-            'target'    => 'giftcard_options',
-            'class'        => array('show_if_gift_card'),
-        );
+        $tabs['frame'] = [
+          'label' => __('Armações', 'woocommerce'),
+          'target' => 'frame_options',
+          'class' => ['show_if_frame'],
+      ];
 
         return $tabs;
     }
 
-
-
-    function giftcard_options_product_tab_content()
+    public function extra_options_product_tab_content()
     {
-?>
-        <div id='giftcard_options' class='panel woocommerce_options_panel'>
+        ?>
+        <div id='lens_options' class='panel woocommerce_options_panel'>
             <div class='options_group'>
                 <?php
-                woocommerce_wp_checkbox(array(
-                    'id'         => '_allow_personal_message',
-                    'label'     => __('Allow the customer to add a personal message', 'woocommerce'),
-                ));
+                woocommerce_wp_checkbox([
+                    'id' => '_allow_personal_message',
+                    'label' => __('Allow the customer to add a personal message', 'woocommerce'),
+                ]); ?>
+            </div>
+        </div>
 
-                woocommerce_wp_text_input(array(
-                    'id'                => '_valid_for_days',
-                    'label'                => __('Gift card validity (in days)', 'woocommerce'),
-                    'desc_tip'            => 'true',
-                    'description'        => __('Enter the number of days the gift card is valid for.', 'woocommerce'),
-                    'type'                 => 'number',
-                    'custom_attributes'    => array(
-                        'min'    => '1',
-                        'step'    => '1',
-                    ),
-                ));
-
-                ?>
+        <div id='frame_options' class='panel woocommerce_options_panel'>
+            <div class='options_group'>
+                <?php
+                woocommerce_wp_checkbox([
+                    'id' => '_allow_personal_message',
+                    'label' => __('Allow the customer to add a personal message', 'woocommerce'),
+                ]); ?>
             </div>
         </div>
 <?php
     }
 
-
-    function save_giftcard_option_fields($post_id)
+    public function save_extra_option_fields($post_id)
     {
+        $_SESSION['my_admin_notices'] = 'TESTE';
 
+        $this->save_lens($post_id);
+        $this->save_frame($post_id);
+    }
+
+    public function my_admin_notices()
+    {
+        if (!empty($_SESSION['my_admin_notices'])) {
+            ?>
+            <div class="notice notice-warning is-dismissible"> 
+              <p><?php echo $_SESSION['my_admin_notices']; ?></p>
+            </div>
+        <?php
+          unset($_SESSION['my_admin_notices']);
+        }
+    }
+
+    private function save_lens($post_id)
+    {
         $allow_personal_message = isset($_POST['_allow_personal_message']) ? 'yes' : 'no';
         update_post_meta($post_id, '_allow_personal_message', $allow_personal_message);
 
-        if (isset($_POST['_valid_for_days'])) :
+        if (isset($_POST['_valid_for_days'])) {
             update_post_meta($post_id, '_valid_for_days', absint($_POST['_valid_for_days']));
-        endif;
+        }
 
-        update_post_meta($post_id, '_redeem_in_stores', (array) $_POST['_redeem_in_stores']);
-
-        $is_gift_card = isset($_POST['_gift_card']) ? 'yes' : 'no';
-        update_post_meta($post_id, '_gift_card', $is_gift_card);
+        update_post_meta($post_id, '_lens', isset($_POST['_lens']) ? 'yes' : 'no');
     }
 
-
-
-    function mytheme_woo_add_custom_variation_fields($loop, $variation_data, $variation)
+    private function save_frame($post_id)
     {
+        $allow_personal_message = isset($_POST['_allow_personal_message']) ? 'yes' : 'no';
+        update_post_meta($post_id, '_allow_personal_message', $allow_personal_message);
 
-        echo '<div class="options_group form-row form-row-full show_if_gift_card">';
+        if (isset($_POST['_valid_for_days'])) {
+            update_post_meta($post_id, '_valid_for_days', absint($_POST['_valid_for_days']));
+        }
+
+        update_post_meta($post_id, '_frame', isset($_POST['_frame']) ? 'yes' : 'no');
+    }
+
+    public function add_extra_product_option($product_type_options)
+    {
+        $product_type_options['lens'] = [
+            'id' => '_lens',
+            'wrapper_class' => 'show_if_simple show_if_variable',
+            'label' => __('Lentes', 'woocommerce'),
+            'description' => __('Dados das lentes.', 'woocommerce'),
+            'default' => 'yes',
+        ];
+
+        $product_type_options['frame'] = [
+          'id' => '_frame',
+          'wrapper_class' => 'show_if_simple show_if_variable',
+          'label' => __('Armações', 'woocommerce'),
+          'description' => __('Dados das armações.', 'woocommerce'),
+          'default' => 'no',
+      ];
+
+        return $product_type_options;
+    }
+
+    public function mytheme_woo_add_custom_variation_fields($loop, $variation_data, $variation)
+    {
+        echo '<div class="options_group form-row form-row-full show_if_lens">';
 
         // Text Field
         woocommerce_wp_text_input(
-            array(
-                'id'          => '_variable_text_field[' . $variation->ID . ']',
-                'label'       => __('Índice de Refração', 'woocommerce'),
+            [
+                'id' => '_variable_text_field['.$variation->ID.']',
+                'label' => __('Índice de Refração', 'woocommerce'),
                 'placeholder' => 'Digite o indice',
-                'desc_tip'    => true,
-                'description' => __("Texto de ajuda.", "woocommerce"),
-                'value' => get_post_meta($variation->ID, '_variable_text_field', true)
-            )
+                'desc_tip' => true,
+                'description' => __('Texto de ajuda.', 'woocommerce'),
+                'value' => get_post_meta($variation->ID, '_variable_text_field', true),
+            ]
         );
 
         echo '</div>';
     }
 
-    function mytheme_woo_add_custom_variation_fields_save($post_id)
+    public function mytheme_woo_add_custom_variation_fields_save($post_id)
     {
-
         // Text Field
         $woocommerce_text_field = $_POST['_variable_text_field'][$post_id];
         update_post_meta($post_id, '_variable_text_field', esc_attr($woocommerce_text_field));
     }
 
-    /**
-     * Add 'Gift Card' product option
-     */
-    function add_gift_card_product_option($product_type_options)
+    public function session_start_admin()
     {
-
-        $product_type_options['gift_card'] = array(
-            'id'            => '_gift_card',
-            'wrapper_class' => 'show_if_simple show_if_variable',
-            'label'         => __('Lentes', 'woocommerce'),
-            'description'   => __('Gift Cards allow users to put in personalised messages.', 'woocommerce'),
-            'default'       => 'no'
-        );
-
-        return $product_type_options;
-    }
-
-
-    private function taxonomy_brand()
-    {
-        $nameSingular = "Marca";
-        $namePlural = "Marcas";
-        $gender = "a";
-
-        $labels = array(
-            'name'                       => "{$namePlural}",
-            'singular_name'              => "{$nameSingular}",
-            'menu_name'                  => "{$namePlural}",
-            'all_items'                  => "Tod{$gender}s {$namePlural}",
-            'parent_item'                => "{$nameSingular} Pai",
-            'parent_item_colon'          => "{$nameSingular} Pai:",
-            'new_item_name'              => "Nov{$gender} {$nameSingular}",
-            'add_new_item'               => "Adicionar nov{$gender} {$nameSingular}",
-            'edit_item'                  => "Editar {$nameSingular}",
-            'update_item'                => "Atualizar {$nameSingular}",
-            'separate_items_with_commas' => "Separar {$nameSingular} com virgulas",
-            'search_items'               => "Procurar {$namePlural}",
-            'add_or_remove_items'        => "Adicionar or remover {$namePlural}",
-            'choose_from_most_used'      => "Escolher d{$gender} mais usad{$gender} {$namePlural}",
-        );
-        $args = array(
-            'labels'                     => $labels,
-            'hierarchical'               => true,
-            'public'                     => true,
-            'show_ui'                    => true,
-            'show_admin_column'          => true,
-            'show_in_nav_menus'          => true,
-            'show_tagcloud'              => true,
-        );
-        register_taxonomy('brand', 'product', $args);
-    }
-    private function taxonomy_material()
-    {
-        $nameSingular = "Material";
-        $namePlural = "Materiais";
-        $gender = "o";
-
-        $labels = array(
-            'name'                       => "{$namePlural}",
-            'singular_name'              => "{$nameSingular}",
-            'menu_name'                  => "{$namePlural}",
-            'all_items'                  => "Tod{$gender}s {$namePlural}",
-            'parent_item'                => "{$nameSingular} Pai",
-            'parent_item_colon'          => "{$nameSingular} Pai:",
-            'new_item_name'              => "Nov{$gender} {$nameSingular}",
-            'add_new_item'               => "Adicionar nov{$gender} {$nameSingular}",
-            'edit_item'                  => "Editar {$nameSingular}",
-            'update_item'                => "Atualizar {$nameSingular}",
-            'separate_items_with_commas' => "Separar {$nameSingular} com virgulas",
-            'search_items'               => "Procurar {$namePlural}",
-            'add_or_remove_items'        => "Adicionar or remover {$namePlural}",
-            'choose_from_most_used'      => "Escolher d{$gender} mais usad{$gender} {$namePlural}",
-        );
-        $args = array(
-            'labels'                     => $labels,
-            'hierarchical'               => true,
-            'public'                     => true,
-            'show_ui'                    => true,
-            'show_admin_column'          => true,
-            'show_in_nav_menus'          => true,
-            'show_tagcloud'              => true,
-        );
-        register_taxonomy('material', 'product', $args);
+        if (!session_id()) {
+            session_start();
+        }
     }
 }
 
