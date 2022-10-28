7629d401f67df99fff0f93f2a13ec8d60f64a5f3
Leandro Ishikawa <lehnihon@hotmail.com>
2022-10-27 22:28:18 -0300

    Botao cart removido

--- includes/class-wc-wottica-cart.php
+++ includes/class-wc-wottica-cart.php
@@ -11,6 +11,8 @@ class WC_Wottica_Cart
     {
         add_filter('woocommerce_get_item_data', [$this, 'display_cart_item_custom_meta_data'], 10, 2);
         add_action('woocommerce_before_calculate_totals', [$this, 'add_custom_price']);
+        add_action('woocommerce_check_cart_items', [$this, 'action_woocommerce_check_cart_items'], 10);
+        add_action('woocommerce_proceed_to_checkout', [$this, 'custom_button_proceed_to_checkout'], 20);
     }
 
     public function display_cart_item_custom_meta_data($item_data, $cart_item)
@@ -31,6 +33,19 @@ class WC_Wottica_Cart
             $custom_price = $cart_item['data']->get_price() + (isset($cart_item['lens']['price']) ? $cart_item['lens']['price'] : 0);
             $cart_item['data']->set_price($custom_price);
         }
+    }
+
+    public function action_woocommerce_check_cart_items()
+    {
+        remove_action('woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20);
+    }
+
+    public function custom_button_proceed_to_checkout()
+    {
+        $link = home_url('/enviar-fotos');
+
+        echo '<a href="'.$link.'" class="checkout-button button alt wc-forward">'.
+        __('Enviar fotos', 'woocommerce').'</a>';
     }
 }
 
